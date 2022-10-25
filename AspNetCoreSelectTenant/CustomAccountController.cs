4559f7d7ad7c1048bff52b378c99b99e703dcaad
damienbod <damien_bod@hotmail.com>
2022-10-25 15:09:33 +0200

    Init login page

---
+++ AspNetCoreSelectTenant/CustomAccountController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetCoreSelectTenant;
+
+[AllowAnonymous]
+[Route("[controller]")]
+public class CustomAccountController : Controller
+{
+    private readonly IConfiguration _configuration;
+
+    public CustomAccountController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpGet("SignIn")]
+    public IActionResult SignIn([FromQuery] string redirectUri)
+    {
+        string redirect;
+        if (!string.IsNullOrEmpty(redirectUri) && Url.IsLocalUrl(redirectUri))
+        {
+            redirect = redirectUri;
+        }
+        else
+        {
+            redirect = Url.Content("~/")!;
+        }
+
+        return Challenge(new AuthenticationProperties { RedirectUri = redirect });
+    }
+}
---
+++ AspNetCoreSelectTenant/Pages/Login.cshtml.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AspNetCoreSelectTenant.Pages;
+
+public class LoginModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
